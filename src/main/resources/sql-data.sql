598d1998301a9c0dd54203e3eb8f0c13e73cb525
Kieran Hart-Brooke <kieranhartbrooke@gmail.com>
2022-02-16 16:41:38 +0000

    added some table constructers to sql files to help write java code. Will delete when finished

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -13,3 +13,24 @@ INSERT INTO `ims`.`order_contents` (`fk_order_id`, `fk_item_id`) VALUES ('1', '1
 INSERT INTO `ims`.`order_contents` (`fk_order_id`, `fk_item_id`) VALUES ('2', '1');
 INSERT INTO `ims`.`order_contents` (`fk_order_id`, `fk_item_id`) VALUES ('2', '2');
 INSERT INTO `ims`.`order_contents` (`fk_order_id`, `fk_item_id`) VALUES ('3', '3');
+
+SELECT o.`id`, c.`first_name`, c.`surname`, i.`name`
+FROM `orders` o 
+JOIN `customers` c ON c.`id` = o.`fk_cust_id`
+JOIN `order_contents` oc ON oc.`fk_order_id` = o.`id`
+JOIN `items` i ON i.`id` = oc.`fk_item_id`;
+
+SELECT o.`id`, c.`first_name`, c.`surname`, GROUP_CONCAT(i.`name`) AS contents
+FROM `orders` o 
+JOIN `customers` c ON c.`id` = o.`fk_cust_id`
+JOIN `order_contents` oc ON oc.`fk_order_id` = o.`id`
+JOIN `items` i ON i.`id` = oc.`fk_item_id`
+GROUP BY o.`id`;
+
+SELECT o.`id`, o.`fk_cust_id`, c.`first_name`, c.`surname`, c.`address`, c.`postcode`, c.`email`
+FROM `orders` o 
+JOIN `customers` c ON c.`id` = o.`fk_cust_id`;
+
+SELECT oc.fk_order_id, oc.fk_item_id, i.name, i.price
+FROM order_contents oc
+JOIN items i ON i.id = oc.fk_item_id; 
